3a16bb168424ae7617629fda0e563040eab2b957
clembnl <cl.bonal@free.fr>
2022-03-26 05:15:07 +0100

    frontend base, login/register ok, proxy and https tbd + MyPage and email reset password + css

---
+++ authentication-ui/src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <header>
+        <img src="../assets/key_icon.svg" alt="key_icon" />
+        <h1> | <a href="/"> Clement Bonal Web Security</a></h1>
+    </header>
+</template>
+
+<style scoped>
+header{
+    display: flex;
+}
+
+h1 {
+    color: white;
+    margin-left: 10px;
+}
+
+a {
+    text-decoration: none;
+    color: white;
+}
+</style>
---
+++ authentication-ui/src/components/SignInForm.vue
@@ -0,0 +1,48 @@
+<template>
+  <form @submit="onSubmit" class="signin-form">
+    <div class="form-control">
+      <label>Username</label>
+      <input type="text" v-model="username" name="username" placeholder="Username" />
+    </div>
+    <div class="form-control">
+      <label>Email</label>
+      <input type="email" v-model="email" name="email" placeholder="Email" />
+    </div>
+    <div class="form-control">
+      <label>Password</label>
+      <input type="password" v-model="password" name="password" placeholder="Password" />
+    </div>      
+
+    <input type="submit" value="Sign In" class="btn" />
+  </form>        
+</template>
+
+<script>
+export default {
+    name: "SignInForm",
+    data() {
+        return {
+            username: "",
+            email: "",
+            password: "",
+        }
+    },
+    methods: {
+        onSubmit(e) {
+            e.preventDefault()
+
+            const user = {
+                username: this.username,
+                email: this.email,
+                password: this.password,
+            }
+
+            this.$emit('signin', user)
+
+            this.username = ""
+            this.email = ""
+            this.password = ""
+        }
+    }
+}
+</script>
